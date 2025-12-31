545a11d716d73b3ebb99ad1e72b6b90a5b185cb6
Mukhammad <nomonhojaevm@gmail.com>
2025-12-31 13:13:56 +0500

    feat: connection mongoDB

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("Web Serverni Boshlash");
+const express = require("express");
+const app = express(); // expressning objecti olindi
+const fs = require("fs");
+
+//MongoDB 
+
+const db = require("./server").db();
+
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
+
+// 1: Kirish code                       // Bu qismdagi use bu => DP => bog'lab beryabdi
+app.use(express.static("public"));      // Clientlar uchun public folderni ochib berdik
+app.use(express.json());                // Kirib kelayotgan json formatni objectga uguradi
+app.use(express.urlencoded({ extended: true }));    // Trad. HTML formdan post qilingan narsalarni togri qabul qiladi
+
+// 2: Session code
+
+// BSSR backend side site rendering || backendda fronted qurish
+// 3: Views code
+app.set("views", "views");      // Folderni korsatdik => 1 o'zgarmaydi 2 folder nomi bilan mos bolishi kerak
+app.set("view engine", "ejs");  // views folder ichidan o'qiydi EJS bilan [biz signal berdik shu engine`da bolaman]
+
+// 4: Routing code      -       qaymoq joyi
+// hamma / ga kelganlarga pastdagi qator chiqadi
+app.get("/", function (req, res) {
+    res.render('reja')
+});
+
+app.post('/create-item', (req, res) => {
+    console.log(req.body);
+    res.json({ test: "succes" });
+})
+    
+app.get('/author', (req, res) => {
+    res.render('author', { user: user })
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,48 +1,23 @@
-console.log("Web Serverni Boshlash");
-const express = require("express");
-const app = express(); // expressning objecti olindi
 const http = require('http');
-const fs = require("fs");
+const mongodb = require('mongodb');
 
+let db;
+const connectionString = "mongodb+srv://Adam:RG3XEzD1AgnQkAe9@cluster0.lwmnslt.mongodb.net/Reja?appName=Cluster0";
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if (err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data);
-    }
-})
-
-// 1: Kirish code                       // Bu qismdagi use bu => DP => bog'lab beryabdi
-app.use(express.static("public"));      // Clientlar uchun public folderni ochib berdik
-app.use(express.json());                // Kirib kelayotgan json formatni objectga uguradi
-app.use(express.urlencoded({ extended: true }));    // Trad. HTML formdan post qilingan narsalarni togri qabul qiladi
-
-// 2: Session code
-
-// BSSR backend side site rendering || backendda fronted qurish
-// 3: Views code
-app.set("views", "views");      // Folderni korsatdik => 1 o'zgarmaydi 2 folder nomi bilan mos bolishi kerak
-app.set("view engine", "ejs");  // views folder ichidan o'qiydi EJS bilan [biz signal berdik shu engine`da bolaman]
-
-// 4: Routing code      -       qaymoq joyi
-// hamma / ga kelganlarga pastdagi qator chiqadi
-app.post('/create-item', (req, res) => {
-    console.log(req.body);
-    res.json({ test: "succes" });
-})
-
-app.get('/author', (req, res) => {
-    res.render('author', { user: user })
-})
-
-app.get("/", function (req, res) {
-    res.render('reja')
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
-});
+mongodb.connect(
+    connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        if (err) {
+            console.log("ERROR on connection mongoDB")
+        } else {
+            console.log("mongoDB connetion succeed");
+            module.exports = client;
+            const app = require('./app');
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+            });
+        };
+    });
